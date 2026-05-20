890a07e53244bd998dc68bb895953aa9af51b2f9
OMauriStkOne <omar@stackone.com>
2026-05-20 12:12:30 +0100

    feat(types): add local webhook event payload types

---
+++ src/types.ts
@@ -0,0 +1,42 @@
+export type HorseColors = {
+  body: string;
+  mane: string;
+  tail: string;
+  saddle: string;
+};
+
+export type RaceCreatedEvent = {
+  event: 'race.created';
+  delivery_id: string;
+  sent_at: string;
+  organisation: { org_id: string; org_name: string };
+  race: {
+    race_id: string;
+    name: string;
+    join_code: string;
+    start_time: string;
+    end_time: string;
+    tz: string;
+  };
+};
+
+export type RaceEndedResult = {
+  rank: number;
+  name: string;
+  final_tokens: number;
+  user_name: string;
+  colors: HorseColors;
+};
+
+export type RaceEndedEvent = {
+  event: 'race.ended';
+  delivery_id: string;
+  sent_at: string;
+  organisation: { org_id: string; org_name: string };
+  race: {
+    race_id: string;
+    name: string;
+    tz: string;
+  };
+  results: RaceEndedResult[];
+};
